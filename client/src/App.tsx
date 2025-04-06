40d72c138d13626183aa292097183699ae71eacc
Brian Smith <brian@brianmatthewsmith.com>
2025-04-06 08:38:26 -0400

    Create basic charts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,194 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  AppBar,
+  Card,
+  CardContent,
+  MenuItem,
+  MenuList,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { BarChart } from "@mui/x-charts";
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const params = new URLSearchParams({
+  pageSize: "1000",
+  format: "json",
+  "filter.advanced": "AREA[StartDate]RANGE[01/01/2024,MAX]",
+}).toString();
+
+type Study = {
+  protocolSection: {
+    conditionsModule: {
+      conditions: string[];
+    };
+    sponsorCollaboratorsModule: {
+      leadSponsor: {
+        name: string;
+      };
+    };
+  };
+};
+
+const getTopStudies = (studies: Study[]) => {
+  const numStudiesBySponsor = studies.reduce<Record<string, number>>(
+    (acc, study) => {
+      const sponsor =
+        study.protocolSection.sponsorCollaboratorsModule.leadSponsor.name;
+
+      acc[sponsor] = (acc[sponsor] ?? 0) + 1;
+
+      return acc;
+    },
+    {}
+  );
+  console.log({ numStudiesBySponsor });
+
+  const topStudiesBySponsor = Object.fromEntries(
+    Object.entries(numStudiesBySponsor)
+      .sort((a, b) => b[1] - a[1])
+      .slice(0, 10)
+  );
+  console.log({ topStudiesBySponsor });
+
+  return topStudiesBySponsor;
+};
+
+const getTopConditions = (studies: Study[]) => {
+  const numConditionsByStudy = studies.reduce<Record<string, number>>(
+    (acc, study) => {
+      const conditions =
+        study.protocolSection.conditionsModule.conditions ?? [];
+
+      conditions.forEach((condition) => {
+        acc[condition] = (acc[condition] ?? 0) + 1;
+      });
+
+      return acc;
+    },
+    {}
+  );
+  console.log({ numConditionsByStudy });
+
+  const topConditionsByStudy = Object.fromEntries(
+    Object.entries(numConditionsByStudy)
+      .sort((a, b) => b[1] - a[1])
+      .slice(0, 10)
+  );
+  console.log({ topConditionsByStudy });
+
+  return topConditionsByStudy;
+};
+
+export const App = () => {
+  const [studies, setStudies] = useState<Study[]>([]);
+
+  useEffect(() => {
+    const getStudies = async () => {
+      const response = await fetch(
+        `https://clinicaltrials.gov/api/v2/studies?${params}`
+      );
+      const json = await response.json();
+
+      setStudies(json.studies);
+    };
+
+    getStudies();
+  }, []);
+  console.log({ studies });
+
+  const numStudiesBySponsor = getTopStudies(studies);
+  const numConditionsByStudy = getTopConditions(studies);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Stack direction="column" flexGrow={1} height="100vh">
+      <AppBar />
 
-export default App
+      <Stack direction="row" flexGrow={1}>
+        <Paper sx={{ width: "200px" }}>
+          <MenuList>
+            <MenuItem>Home</MenuItem>
+          </MenuList>
+        </Paper>
+
+        <Stack p={4} gap={4}>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography
+                component="h2"
+                variant="subtitle1"
+                align="center"
+                fontWeight={700}
+              >
+                2025 Clinical Trials: Top 10 Industry Sponsors
+              </Typography>
+              <BarChart
+                width={1200}
+                height={500}
+                borderRadius={8}
+                colors={["#02b2af"]}
+                xAxis={[
+                  {
+                    scaleType: "band",
+                    data: Object.keys(numStudiesBySponsor),
+                  },
+                ]}
+                series={[
+                  {
+                    label: "Number of Studies",
+                    data: Object.values(numStudiesBySponsor),
+                    type: "bar",
+                  },
+                ]}
+                grid={{ horizontal: true }}
+                slotProps={{
+                  legend: {
+                    hidden: true,
+                  },
+                }}
+              />
+            </CardContent>
+          </Card>
+
+          <Card variant="outlined">
+            <CardContent>
+              <Typography
+                component="h2"
+                variant="subtitle1"
+                align="center"
+                fontWeight={700}
+              >
+                2025 Clinical Trials: Top 10 Conditions
+              </Typography>
+              <BarChart
+                width={1200}
+                height={500}
+                borderRadius={8}
+                colors={["#02b2af"]}
+                xAxis={[
+                  {
+                    scaleType: "band",
+                    data: Object.keys(numConditionsByStudy),
+                  },
+                ]}
+                series={[
+                  {
+                    label: "Number of Conditions",
+                    data: Object.values(numConditionsByStudy),
+                    type: "bar",
+                  },
+                ]}
+                grid={{ horizontal: true }}
+                slotProps={{
+                  legend: {
+                    hidden: true,
+                  },
+                }}
+              />
+            </CardContent>
+          </Card>
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+};
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import { App } from "./App.tsx";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
